df0f24c653c385ddaaccdce39235a52f337af19c
Justin Bermudez <justinanbermudez@gmail.com>
2020-01-26 10:38:40 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,3 +22,9 @@ end
 def list(songs)
   songs.each_with_index{|song, index| puts "#{index + 1}. #{song}"}
 end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+  
+end
