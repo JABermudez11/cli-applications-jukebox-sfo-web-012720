8d483b9c48c4a4c8767d8d0e725bf217a579b346
Justin Bermudez <justinanbermudez@gmail.com>
2020-01-26 10:47:21 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -46,7 +46,9 @@ def exit_jukebox
 end
 
 def run(songs)
-  puts "Please enter a command:"
-  input = gets.strip
-  
+  loop do
+    puts "Please enter a command:"
+    input = gets.strip
+  end
+
 end
